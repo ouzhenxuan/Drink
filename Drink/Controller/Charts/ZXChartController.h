526e1437a0e9a23a44fe5e161b778a76e8f75227
ozx <442910407@qq.com>
2019-04-23 01:49:11 +0800

    t添加charts图标，实例化图标

--- Drink/Controller/Charts/ZXChartController.h
+++ Drink/Controller/Charts/ZXChartController.h
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DemoBaseViewController.h"
+#import "Charts-Swift.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ZXChartController : UIViewController
+@interface ZXChartController : DemoBaseViewController
 
 @end
+
+
 
 NS_ASSUME_NONNULL_END
---
+++ Drink/Supporting Files/demo2.swift
@@ -0,0 +1,9 @@
+//
+//  demo2.swift
+//  Drink
+//
+//  Created by 区振轩 on 2019/4/23.
+//  Copyright © 2019 区振轩. All rights reserved.
+//
+
+import Foundation
